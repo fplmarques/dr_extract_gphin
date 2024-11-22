aa3b6d628b5a57da663449cddc0c6503180c7c45
Fernando Portella de Luna Marques <fernando@ib.usp.br>
2024-11-22 14:28:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,49 @@
 import streamlit as st
+from openai import OpenAI
+from datetime import datetime
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+# Get the current date and format it as m/d/yyyy
+date_str = datetime.now().strftime("%m/%d/%Y")
+
+client = OpenAI(api_key=OPENAI_IPA_KEY)
+
+# Read the content from a text file
+with open('gpt4_dr2tracker_preprompt.txt', 'r') as file:
+    preprompt = file.read()
+
+# Replace the date "11/08/2024" in the preprompt with the current date
+drtracker_preprompt = preprompt.replace("11/08/2024", date_str)
+
+# Initialize the messages structure
+messages = [
+    {
+        "role": "system",
+        "content": drtracker_preprompt
+    }
+]
+
+# Define the function for interaction
+def CustomChatGPT(user_input):
+    if len(messages) > 1:
+        messages.pop()
+
+    messages.append({"role": "user", "content": user_input})
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+    )
+    ChatGPT_reply = response.choices[0].message.content
+    return ChatGPT_reply
+
+# Streamlit UI elements
+st.title("AI DRtrack Builder: from HTML to TSV")
+
+user_input = st.text_area("Enter the HTML code for the Daily Report:", height=300)
+
+if st.button("Submit"):
+    if user_input:
+        response = CustomChatGPT(user_input)
+        st.text_area("Response", response, height=200)
+    else:
+        st.warning("Please enter some input before submitting.")
